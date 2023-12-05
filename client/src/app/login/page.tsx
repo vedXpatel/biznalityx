7d46dbad7d438109e9ccd5fa395d5e89eb45680f
vedXpatel <ved.gp@somaiya.edu>
2023-12-05 20:04:36 +0530

    :rocket: prettier configuration done

--- client/src/app/login/page.tsx
+++ client/src/app/login/page.tsx
@@ -1,12 +1,14 @@
-import {RegisterLink, LoginLink} from "@kinde-oss/kinde-auth-nextjs/server";
+import { RegisterLink, LoginLink } from "@kinde-oss/kinde-auth-nextjs/server";
 
 export default function Login() {
-    return(
-<>
-    <h1>
-    <LoginLink>Sign in</LoginLink>
-    </h1>
-    <RegisterLink>Sign up</RegisterLink>
-</>
-    )
+  return (
+    <>
+      <h1>
+        <LoginLink>Sign in</LoginLink>
+      </h1>
+      <h1>
+        <RegisterLink>Sign up</RegisterLink>
+      </h1>
+    </>
+  );
 }
