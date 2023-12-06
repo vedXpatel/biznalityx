21a041c741321dca13bdb274538bffadfa20d135
vedXpatel <ved.gp@somaiya.edu>
2023-12-06 17:03:38 +0530

    :rocket: Updated Dashboard

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -1,24 +1,30 @@
-'use client';
-import Map from '../../components/map';
-
+"use client";
+import Map from "../../components/map";
+import Stats from "../../components/stats";
+import {Input} from "@nextui-org/react";
 
 export default function Dashboard() {
   return (
     <>
-      <div
-        style={{
-          width: "20vw",
-          height: "100vh",
-          display: "flex",
-          flexDirection: "column",
-          backgroundColor: "white",
-          position: "relative",
-          left: 0,
-          borderTopRightRadius: 20,
-          borderBottomRightRadius: 20,  
-        }}
-      >
-        <Map/>
+      <div className="" style={{ display: "flex", flexDirection: "row" }}>
+        <div
+          style={{
+            width: "20vw",
+            height: "100vh",
+            display: "flex",
+            flexDirection: "column",
+            backgroundColor: "white",
+            position: "relative",
+            left: 0,
+            borderTopRightRadius: 20,
+            borderBottomRightRadius: 20,
+          }}
+        ></div>
+          <div className="" style={{display:"flex",flexDirection:'column', width:'100%',paddingBottom:'20px'}}>
+              <Input style={{margin:'10px'}} color={"primary"}/>
+        <Stats />
+        <Map />
+          </div>
       </div>
     </>
   );
--- client/src/components/map.tsx
+++ client/src/components/map.tsx
@@ -1,32 +1,50 @@
 import React from "react";
-import GoogleMapReact from 'google-map-react';
+import GoogleMapReact from "google-map-react";
 
 interface ReactComponent {
-    text: string;
+  text: string;
+}
+
+interface Map {
+  lat: number;
+  lng: number;
+  centreLat?: number;
+  centreLong?: number;
 }
 
 const AnyReactComponent = ({ text }: ReactComponent) => <div>{text}</div>;
 
-export default function SimpleMap(){
+export default function SimpleMap() {
   const defaultProps = {
     center: {
-      lat: 10.99835602,
-      lng: 77.01502627
+      lat: 40.7128,
+      lng: -74.006,
     },
-    zoom: 11
+    zoom: 11,
   };
 
   return (
     // Important! Always set the container height explicitly
-    <div style={{ height: '100vh', width: '100%' }}>
+    <div
+      style={{
+        height: "63vh",
+        position: "relative",
+        // top: "35vh",
+        margin: "10px",
+        borderRadius: "10px",
+        width: "100%",
+      }}
+    >
       <GoogleMapReact
-        bootstrapURLKeys={{ key: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY as string }}
+        bootstrapURLKeys={{
+          key: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY as string,
+        }}
         defaultCenter={defaultProps.center}
         defaultZoom={defaultProps.zoom}
       >
         <AnyReactComponent
-          lat={59.955413}
-          lng={30.337844}
+          lat={10.99835602}
+          lng={77.01502627}
           text="My Marker"
         />
       </GoogleMapReact>
