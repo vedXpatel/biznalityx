72cbcf64a50d177799b2a1c6ea4d4f598fa92fd4
vedXpatel <ved.gp@somaiya.edu>
2023-12-06 20:32:55 +0530

    :rocket: Dashboard UI refreshed

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -1,31 +1,99 @@
 "use client";
 import Map from "../../components/map";
 import Stats from "../../components/stats";
-import {Input} from "@nextui-org/react";
+import {Input, Button} from "@nextui-org/react";
 
 export default function Dashboard() {
-  return (
-    <>
-      <div className="" style={{ display: "flex", flexDirection: "row" }}>
-        <div
-          style={{
-            width: "20vw",
-            height: "100vh",
-            display: "flex",
-            flexDirection: "column",
-            backgroundColor: "white",
-            position: "relative",
-            left: 0,
-            borderTopRightRadius: 20,
-            borderBottomRightRadius: 20,
-          }}
-        ></div>
-          <div className="" style={{display:"flex",flexDirection:'column', width:'100%',paddingBottom:'20px'}}>
-              <Input style={{margin:'10px'}} color={"primary"}/>
-        <Stats />
-        <Map />
-          </div>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className="" style={{display: "flex", flexDirection: "row"}}>
+                <div
+                    style={{
+                        width: "20vw",
+                        height: "100vh",
+                        display: "flex",
+                        flexDirection: "column",
+                        backgroundColor: "white",
+                        background: 'radial-gradient(circle, rgba(4,4,4,1) 0%, rgba(68,1,59,1) 0%, rgba(0,0,0,1) 100%)',
+                        position: "relative",
+                        left: 0,
+                        borderTopRightRadius: 20,
+                        borderBottomRightRadius: 20,
+                        paddingTop:'10%'
+                    }}
+                >
+                    <Button variant='light' color='danger' style={{borderRadius:'30px',height:'7vh',fontSize:'20px',margin:15,}}>Home</Button>
+                   <Button variant='shadow' color='danger' style={{borderRadius:'30px',height:'7vh',fontSize:'20px',margin:15,}}>Dashboard</Button>
+                    <Button variant='light' color='danger' style={{borderRadius:'30px',height:'7vh',fontSize:'20px',margin:15,}}>Properties</Button>
+                    <Button variant='light' color='danger' style={{borderRadius:'30px',height:'7vh',fontSize:'20px',margin:15,}}>SCM</Button>
+                    <Button variant='light' color='danger' style={{position:"relative",top:"30%",borderRadius:'30px',height:'7vh',fontSize:'20px',margin:15,}}>LogOut</Button>
+                </div>
+                <div className=""
+                     style={{display: "flex", flexDirection: 'column', width: '100%', paddingBottom: '20px'}}>
+                    <div className="input" style={{paddingLeft:'50px',paddingRight:'50px',marginTop:'20px',marginBottom:'20px'}}>
+                        <Input
+                            label="Search"
+                            isClearable
+                            radius="lg"
+                            classNames={{
+                                label: "text-black/50 dark:text-white/90",
+                                input: [
+                                    "bg-transparent",
+                                    "text-black/90 dark:text-white/90",
+                                    "placeholder:text-default-700/50 dark:placeholder:text-white/60",
+                                ],
+                                innerWrapper: "bg-transparent",
+                                inputWrapper: [
+                                    "shadow-xl",
+                                    "bg-default-200/50",
+                                    "dark:bg-default/60",
+                                    "backdrop-blur-xl",
+                                    "backdrop-saturate-200",
+                                    "hover:bg-default-200/70",
+                                    "dark:hover:bg-default/70",
+                                    "group-data-[focused=true]:bg-default-200/50",
+                                    "dark:group-data-[focused=true]:bg-default/60",
+                                    "!cursor-text",
+                                ],
+                            }}
+                            placeholder="Type to search..."
+                            startContent={
+                                <svg
+                                    aria-hidden="true"
+                                    fill="none"
+                                    focusable="false"
+                                    height="1em"
+                                    role="presentation"
+                                    viewBox="0 0 24 24"
+                                    width="1em"
+                                >
+                                    <path
+                                        d="M11.5 21C16.7467 21 21 16.7467 21 11.5C21 6.25329 16.7467 2 11.5 2C6.25329 2 2 6.25329 2 11.5C2 16.7467 6.25329 21 11.5 21Z"
+                                        stroke="currentColor"
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth="2"
+                                    />
+                                    <path
+                                        d="M22 22L20 20"
+                                        stroke="currentColor"
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth="2"
+                                    />
+                                </svg>
+                            }
+                        />
+                    </div>
+                    <div className="" style={{display: 'flex', flexDirection: "row", padding: '10px',alignItems:'center',marginLeft:'2vw'}}>
+                        <Stats title='Total Stores' data={350}/>
+                        <Stats title='Total Population' data={10000000}/>
+                        <Stats title='Average Income' data={500000}/>
+                        <Stats title='Most Populated Area' data={1}/>
+                    </div>
+                    <Map/>
+                </div>
+            </div>
+        </>
+    );
 }
--- client/src/components/stats.tsx
+++ client/src/components/stats.tsx
@@ -1,26 +1,22 @@
 import React from "react";
 import {Card, CardFooter, Image, Button} from "@nextui-org/react";
 
-export default function Stats() {
+interface CardData {
+  title?: string;
+  data?: number;
+  imageSrc?: string;
+}
+
+export default function Stats({title, data, imageSrc}: CardData) {
   return (
     <Card
       isFooterBlurred
       radius="lg"
       className="border-none"
+      style={{width:'18vw',minHeight:'22vh', marginRight:'20px',padding:'10px',background:''}}
     >
-      <Image
-        alt="Woman listing to music"
-        className="object-cover"
-        height={200}
-        src="/images/hero-card.jpeg"
-        width={200}
-      />
-      <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
-        <p className="text-tiny text-white/80">Available soon.</p>
-        <Button className="text-tiny text-white bg-black/20" variant="flat" color="default" radius="lg" size="sm">
-          Notify me
-        </Button>
-      </CardFooter>
+      <h1 style={{fontSize:'25px',marginTop:'1vh'}}>{title}</h1>
+      <h1 style={{fontSize:'40px',fontWeight:'bold', position:"relative",top:'1.5vh',left:'10%'}}>{data}</h1>
     </Card>
   );
 }
